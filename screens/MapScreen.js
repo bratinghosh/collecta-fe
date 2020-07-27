0c30f1d1eae2b5f58bb12eef8f06b3ce48879cc2
Bratin Ghosh <ghosh.bratin13@gmail.com>
2020-07-28 04:44:37 +0800

    Added fine location access(android), Basic react native maps function test

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,15 +1,25 @@
 import React, { useState } from 'react';
 import { StyleSheet } from 'react-native';
-import { Button, Container, Icon, View} from 'native-base';
+import { Button, Container, Icon} from 'native-base';
 import { Snackbar } from 'react-native-paper';
-import MapView from 'react-native-maps';
+import MapView, { Marker, Circle } from 'react-native-maps';
 
 const MapScreen = (props) => {
-  const [snackbarVisible, setSnackbarVisible] = useState(false);
+  const [couponSnackbarVisible, setCouponSnackbarVisible] = useState(false);
+  const [checkpointSnackbarVisible, setCheckpointSnackbarVisible] = useState(false);
 
   return (
     <Container style={styles.container}>
-      <MapView provider={null} style={styles.map}>
+      <MapView provider={null} style={styles.map} showsUserLocation={true}>
+        {/* {<Marker coordinate={{latitude: 22.28, longitude: 114.13}} title="Checkpoint">
+          <Icon type="FontAwesome5" name="search-location" style={{color: '#52307c'}}/>
+        </Marker>
+        <Marker coordinate={{latitude: 22.287, longitude: 114.135}} title="Shop">
+          <Icon type="MaterialIcons" name="location-on" style={{color: '#ff0000'}}/>
+        </Marker>
+        <Circle center={{latitude: 22.285, longitude: 114.133}} radius={75} strokeColor="#52307c33" fillColor="#52307c33"/>
+        <Circle center={{latitude: 22.285, longitude: 114.134}} radius={75} strokeColor="#52307c33" fillColor="#52307c33"/>
+        <Circle center={{latitude: 22.282, longitude: 114.134}} radius={75} strokeColor="#52307c33" fillColor="#52307c33"/>} */}
       </MapView>
       <Button style={styles.menuButton} onPress={() => (props.navigation.openDrawer())}>
         <Icon type="FontAwesome" name="bars" style={styles.buttonIcon}/>
@@ -20,8 +30,11 @@ const MapScreen = (props) => {
       <Button style={styles.centerMapButton} onPress={() => {}}>
         <Icon type="FontAwesome5" name="crosshairs" style={styles.buttonIcon}/>
       </Button>
-      <Snackbar action={{label: 'Show Me', onPress: () => {props.navigation.navigate('My Collection')}}} visible={snackbarVisible} onDismiss={() => {setSnackbarVisible(!snackbarVisible)}} style={styles.snackbar} duration={5000}>
+      <Snackbar action={{label: 'Show Me', onPress: () => {props.navigation.navigate('My Collection')}}} visible={couponSnackbarVisible} onDismiss={() => {setCouponSnackbarVisible(!couponSnackbarVisible)}} style={styles.snackbar} duration={5000}>
         You have discovered a new Coupon!
+      </Snackbar>
+      <Snackbar visible={checkpointSnackbarVisible} onDismiss={() => {setCheckpointSnackbarVisible(!checkpointSnackbarVisible)}} style={styles.snackbar} duration={5000}>
+        You have reached a Checkpoint!
       </Snackbar>
     </Container>
   );
